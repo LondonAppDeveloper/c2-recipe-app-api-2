ab4e62ba3829f372c023a9c3d6a60549b52f50bb
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-16 11:01:54 +0100

    Added test which fails.

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,8 @@ Calculator functions
 def add(x, y):
     """Add x and y and return result."""
     return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    pass
--- app/app/tests.py
+++ app/app/tests.py
@@ -14,3 +14,9 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
