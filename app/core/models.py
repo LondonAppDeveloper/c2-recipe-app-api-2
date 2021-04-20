c37c1f97a1cb3a251849c7ca56503041514ac1e6
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-20 17:08:27 +0100

    Updated create_user() to check email is provided.

--- app/core/models.py
+++ app/core/models.py
@@ -14,6 +14,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
