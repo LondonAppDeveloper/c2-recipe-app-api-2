cc8cf94b5b48172503bde9791c4dec4f355e0ba9
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-20 17:07:54 +0100

    Added test for invalid email.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -31,3 +31,8 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
