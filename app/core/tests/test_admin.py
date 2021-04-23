4e3bb8836a5ae30645620005cf495938400268fe
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-23 15:25:53 +0100

    Added test for edit user page.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -31,3 +31,10 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
