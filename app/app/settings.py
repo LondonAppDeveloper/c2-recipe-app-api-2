f8334ed2d94412dd43e339f14af80719d3a9569c
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-23 16:29:24 +0100

    Added config for drf-spectacular.

--- app/app/settings.py
+++ app/app/settings.py
@@ -131,3 +131,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
