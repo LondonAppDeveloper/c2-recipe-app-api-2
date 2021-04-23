2ed64796ddc90945bffe209deb2a4bc0938f91d6
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-23 15:20:28 +0100

    Make Django admin list users.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
