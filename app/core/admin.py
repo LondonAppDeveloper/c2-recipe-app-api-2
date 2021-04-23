cbdc7fcd69ef94e62d3427f43bc8959ff9050265
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-23 15:46:13 +0100

    Configured add page.

--- app/core/admin.py
+++ app/core/admin.py
@@ -28,6 +28,20 @@ class UserAdmin(BaseUserAdmin):
         (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
