e6e15d5b27c6094b7d9e81694e3f1bf8866b9f1d
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-25 19:08:13 +0100

    Added rest_framework.authtoken to settings.

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
     'core',
 ]
