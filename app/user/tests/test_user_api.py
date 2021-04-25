8965001faff9286ec42871a8c4bc01cbc765b557
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-25 18:49:04 +0100

    Use create_user instead of create method.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -45,7 +45,7 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name',
         }
-        get_user_model().objects.create(**payload)
+        get_user_model().objects.create_user(**payload)
         res = self.client.post(CREATE_USER_URL)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
@@ -72,7 +72,7 @@ class PublicUserApiTests(TestCase):
             'email': 'test@example.com',
             'password': 'test-user-password123',
         }
-        get_user_model().objects.create(**user_details)
+        get_user_model().objects.create_user(**user_details)
 
         payload = {
             'email': user_details['email'],
@@ -85,7 +85,7 @@ class PublicUserApiTests(TestCase):
 
     def test_create_token_bad_credentials(self):
         """Test returns error if credentials invalid."""
-        get_user_model().objects.create(
+        get_user_model().objects.create_user(
             email='test@example.com',
             password='goodpass',
         )
