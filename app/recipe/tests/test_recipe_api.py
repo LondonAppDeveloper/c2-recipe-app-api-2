e832ff2cebb45678933b90ac7c042a962ba6f1d1
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-04 09:35:46 +0100

    Add description field.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -24,6 +24,7 @@ def create_recipe(user, **params):
         'title': 'Sample recipe title',
         'time_minutes': 22,
         'price': Decimal('5.25'),
+        'description': 'Sample description',
         'link': 'http://example.com/recipe.pdf',
     }
     defaults.update(params)
