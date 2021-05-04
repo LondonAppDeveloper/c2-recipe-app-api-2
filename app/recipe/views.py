369c8e4f835c5c7937192c3fcd716748967186b5
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-04 14:33:21 +0100

    Use detail serializer if not listing.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -22,7 +22,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
-        if self.action == 'retrieve':
+        if self.action in ('retrieve', 'update', 'partial_update', 'destroy'):
             return serializers.RecipeDetailSerializer
 
         return self.serializer_class
