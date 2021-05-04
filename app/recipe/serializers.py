514d1c5877a6ad3d297e3826879ac5ae238517a3
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-04 10:35:26 +0100

    Add recipe detail view.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -13,3 +13,10 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -19,3 +19,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
