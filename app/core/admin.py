6b6ee12c1918bef4617aede386401bd283861341
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-06 16:06:11 +0100

    Added tag model.

--- app/core/admin.py
+++ app/core/admin.py
@@ -46,3 +46,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Recipe)
+admin.site.register(models.Tag)
--- app/core/models.py
+++ app/core/models.py
@@ -56,6 +56,19 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
