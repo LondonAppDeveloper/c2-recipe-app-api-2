8cf8d05631d116091cd3776d8fa88c251d781846
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-12 18:07:42 +0100

    Implement delete ingredient API.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -54,7 +54,8 @@ class TagViewSet(mixins.DestroyModelMixin,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngredientViewSet(mixins.UpdateModelMixin,
+class IngredientViewSet(mixins.DestroyModelMixin,
+                        mixins.UpdateModelMixin,
                         mixins.ListModelMixin,
                         viewsets.GenericViewSet):
     """Manage ingredients in the database."""
