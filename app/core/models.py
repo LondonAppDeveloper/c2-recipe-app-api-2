bb11a146289864606025213731dd6a303370bc45
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-12 16:30:57 +0100

    Add Ingredient model.

--- app/core/models.py
+++ app/core/models.py
@@ -72,3 +72,15 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient for recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
