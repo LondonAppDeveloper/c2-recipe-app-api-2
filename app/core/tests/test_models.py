3931f365c41f49d94cd86cc7a0b865939d43e25a
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-12 16:27:56 +0100

    Add tests for ingredient model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -78,3 +78,13 @@ class ModelTests(TestCase):
         tag = models.Tag.objects.create(user=user, name='Tag1')
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
