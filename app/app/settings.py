31a322d42f4744bb9314e95fff6782d041fe2956
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-14 16:00:33 +0100

    Improved settings.

--- app/app/settings.py
+++ app/app/settings.py
@@ -141,5 +141,8 @@ AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
     'COMPONENT_SPLIT_REQUEST': True,
 }
