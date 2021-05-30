f4528bc39975a1b358d2c86c76c68b63a87befdf
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-30 16:18:50 +0100

    Only add media URL in debug mode.

--- app/app/urls.py
+++ app/app/urls.py
@@ -34,4 +34,10 @@ urlpatterns = [
     ),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
